39026551d32b7919671015c72c2f0a3144b1f4fb
noueliah <noelittz@gmail.com>
2022-10-28 17:00:42 -0300

    funcionalidad terminada supuestamente

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,34 +2,45 @@
 import React from 'react';
 import { useState } from "react";
 import Todoform from './Todoform';
-import Todolist from './Todolist';
 import { RiCheckboxMultipleFill } from 'react-icons/ri';
 import { RiCheckboxMultipleLine } from 'react-icons/ri';
 
-function Todo( items, completeItem, removeItem ) {
+function Todo(items, completeItem, removeItem, updateItem) {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   })
 
+  const submitUpdate = value => {
+    updateItem(edit.id, value)
+    setEdit({
+      id: null,
+      value: '',
+    })
+  }
+
+  if (edit.id) {
+    return < Todoform edit={edit} onSubmit={submitUpdate} />
+  }
+
   return items.map((item, index) => (
     <div className={item.isComplete ? 'item-row complete' : 'item-row'} key={index}>
 
-    <div key={item.id} onClick={ () => completeItem(item.id)}>
-      {item.text}
-    </div>
+      <div key={item.id} onClick={() => completeItem(item.id)}>
+        {item.text}
+      </div>
 
-    <div className='icons'>
-     <RiCheckboxMultipleLine 
-     onClick={() => removeItem(item.id)}
-     className='delete-icon'
-     />
+      <div className='icons'>
+        <RiCheckboxMultipleLine
+          onClick={() => removeItem(item.id)}
+          className='delete-icon'
+        />
 
-     <RiCheckboxMultipleFill
-     onClick={() => setEdit ({id: item.id, value: item.text })}
-     className='edit-icon'
-     />
-    </div>
+        <RiCheckboxMultipleFill
+          onClick={() => setEdit({ id: item.id, value: item.text })}
+          className='edit-icon'
+        />
+      </div>
 
     </div>
   ));
--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function Todoform(props) {
-  
-  const [input, setInput] = useState('')
+
+  const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
 
   const handleChange = e => {
     setInput(e.target.value);
@@ -13,22 +19,40 @@ function Todoform(props) {
 
     props.onSubmit({
 
-        id: Math.floor(Math.random()*99999),
-        text: input,
+      id: Math.floor(Math.random() * 99999),
+      text: input,
 
     });
 
     setInput('');
   };
-  
-    return (
+
+  return (
     <form className='todo-form' onSubmit={handleSubmit}>
-        <input type='text'
-        placeholder='Agregar item'
-        value={input} name='text'
-        className='todo-input'
-        onChange={handleChange} />
-        <button className='todo-button'>Agregar</button>
+      {props.edit ? (
+        <>
+          <input type='text'
+            placeholder='Actualizar item'
+            value={input} name='text'
+            className='todo-input'
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className='todo-button'>Actualizar</button> </>) :
+        (<>
+          <input type='text'
+            placeholder='Agregar item'
+            value={input}
+            name='text'
+            className='todo-input'
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className='todo-button'>Agregar</button>
+
+        </>)}
+
+
     </form>
   )
 }
